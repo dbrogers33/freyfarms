6e6f7998411958a45afab8bc4f41e724f13f4f0d
Denham Rogers <denhamrogers10@gmail.com>
2020-04-27 18:08:33 -0500

    updated title

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ import Instagram from "../components/instagram"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO
-      title="Frey Farms | Home to watermelons, pumpkins, and Sara's Homegrown"
+      title="Frey Farms | Producer of watermelons, pumpkins, and Sara's Homegrown"
       description="It is our vision to bring our love of the farm to all families. Whether it is one of our farm grown pumpkins, watermelons, or one of our beverages juiced from our produce, we want you to enjoy the very best."
       image=""
   />
