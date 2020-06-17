b3a49a5c222fb2d89ea438bc67bcf9eac86d04ae
dbrogers33 <denhamrogers10@gmail.com>
2020-06-17 10:28:52 -0500

    updated location page styling and naming

--- src/templates/location.js
+++ src/templates/location.js
@@ -1,22 +1,54 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import Layout from "../components/layout"
+import Hero from "../components/hero"
+
+import SideBySide from '../components/side-by-side-map'
+
 export default ({ data }) => {
 
-    return (
-    <div>
-        <h1>{data.airtable.data.Store_Name}</h1>
-        {/*  <img src={data.airtable.column_name_2} /> */}
-    </div>
-    )
+  return (
+    <Layout>
+      <Hero headline={"Frey Farms Products in " + data.airtable.data.City + ", " + data.airtable.data.State } heroImage={data.hero.childImageSharp.fluid} />
+      <SideBySide
+        headerThree="Get Farm Fresh at..."
+        headerTwo={data.airtable.data.Store_Name + " in " +  data.airtable.data.City + ", " + data.airtable.data.State}
+        street={data.airtable.data.Street_Address}
+        city={data.airtable.data.City + ", " + data.airtable.data.State + " " + data.airtable.data.Zip}
+        phone={data.airtable.data.Phone}
+        src={data.family.childImageSharp.fluid}
+        alt="Sarah Frey sitting in a pumpkin field"
+      />
+    </Layout>
+  )
 }
 
 export const query = graphql`
-query GetRecord($recordId: String!){
-    airtable(recordId: { eq: $recordId}) {
-        data {
-          Store_Name
-        }
-        recordId
+query GetRecord($recordId: String!) {
+  family: file(relativePath: { eq: "family.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
       }
+    }
+  }
+  hero: file(relativePath: { eq: "hero-images/story-hero.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 1500, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+  airtable(recordId: { eq: $recordId}) {
+      data {
+        Store_Name
+        State
+        City
+        Zip
+        Street_Address
+        Phone
+      }
+      recordId
+    }
 }`
