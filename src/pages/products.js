2b9d18beff3e2ee0e857afa4b120f25015bb7369
dbrogers33 <denhamrogers10@gmail.com>
2024-06-26 19:45:11 -0500

    updated product hero

---
+++ src/pages/products.js
@@ -0,0 +1,127 @@
+import React from "react"
+import styled from "styled-components";
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import Hero from "../components/hero"
+import SideBySide from "../components/side-by-side"
+import Button from "../components/button"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO
+      title="Our Products | Frey Farms | Sarah's Homegrown and Tsamma Juice"
+      description="Whether it is one of our farm grown pumpkins, watermelons, or one of our beverages juiced from our produce, we want you to enjoy the very best."
+    />
+    <Hero headline="sharing our love of the farm with you." heroImage={data.hero.childImageSharp.fluid} />
+    
+    <Products>
+    <ProductWrapper>
+      <SideBySide reverse="row-reverse"
+        headerThree=""
+        headerTwo=""
+        paragraph="Frey Farms controls the entire process from planting and harvesting to processing, packing and shipping. Our Team works closely with customers on both transportation efficiencies and food safety protocols to maintain a strong reputation in the industry. Frey includes customers in supply chain transparency from developing new seed varieties to harvest schedules and logistics. Every step in the process is overseen by our team, guaranteeing our customers receive superior service and high-quality pr."
+        src={data.produce.childImageSharp.fluid}
+        alt="Sarah's Homegrown Produce'"
+      >
+        {/* <Button link="/produce-guide.pdf" buttonLabel="Our Produce Guide" /> */}
+      </SideBySide>
+    </ProductWrapper>
+    
+    <ProductWrapper>
+      <SideBySide
+        headerThree="Sarah's Homegrown"
+        headerTwo="Beverages"
+        paragraph="We have a variety of flavors available and are always expanding our offerings. Learn more about our fresh from the farm beverages."
+        src={data.beverages.childImageSharp.fluid}
+        alt="Sarah's Homegrown Produce'"
+      >
+        <ExternalLink href="http://drinksarahs.com" target="_blank">Visit DrinkSarahs.com</ExternalLink><br /><br />
+        <Button link="/tsamma-shg-sellsheets.pdf" buttonLabel="View Our Spec Sheet" />
+      </SideBySide>
+    </ProductWrapper>
+    
+    <ProductWrapper>
+      <SideBySide reverse="row-reverse"
+        headerThree="Tsamma"
+        headerTwo="Watermelon Juice"
+        paragraph="Watermelon has been a source of natural, refreshing hydration for thousands of years. At Frey Farms we are committed to the growth and science of watermelon,
+        and to the best practices to create leading watermelon products. Each bottle of Tsamma® has the taste of a juicy watermelon and is packed with immune boosting nutrients. Citrulline is naturally found in watermelon and improves blood flow, allowing your body to absorb nutrients and hydrate faster."
+        src={data.tsamma.childImageSharp.fluid}
+        alt="Sarah's Homegrown Produce'"
+      >
+      <ExternalLink href="http://tsammajuice.com" target="_blank">Visit TsammaJuice.com</ExternalLink><br /><br />
+      <Button link="/tsamma-shg-sellsheets.pdf" buttonLabel="View Our Spec Sheet" />
+      </SideBySide>
+    </ProductWrapper>
+    </Products>
+
+  </Layout>
+
+  
+)
+
+const Products = styled.div`
+  
+`
+const ExternalLink = styled.a`
+    text-transform: uppercase;
+    font-family: 'Cervo Neue';
+    color: #0F5800;
+    font-weight: 800;
+    font-size: 18px;
+    margin: .1em 0;
+    text-decoration: none;
+    & :hover {
+      text-decoration: underline;
+    }
+    @media (min-width: 800px) {
+        font-size: 20px;
+    }
+`
+const ProductWrapper = styled.section`
+  @media (min-width: 900px) {
+    margin: 3em;
+    box-shadow: 10px 4px 34px rgba(0, 0, 0, 0.15);
+    & > .wrapper {
+    
+    }
+  }
+`
+
+export const query = graphql`
+query {
+  hero: file(relativePath: { eq: "hero-images/products-hero.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1500, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp_noBase64
+      }
+    }
+  }
+  produce: file(relativePath: { eq: "products/produce.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+  beverages: file(relativePath: { eq: "products/beverages.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+  tsamma: file(relativePath: { eq: "products/tsamma.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+}
+`
+
+export default IndexPage
