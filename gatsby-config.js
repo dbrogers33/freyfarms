b8c4628da8881d325986019964661111f33102f1
Denham Rogers <denhamrogers10@gmail.com>
2025-07-22 20:39:13 -0500

    maybe fixed packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,14 +8,6 @@ module.exports = {
     siteUrl: `https://freyfarms.com`,
     mapbox_api_key: process.env.GATSBY_MAPBOX_API_KEY,
   },
-  adapters: [
-    {
-      resolve: 'gatsby-adapter-netlify',
-      options: {
-        // You can leave this empty or add custom options
-      },
-    },
-  ],
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
