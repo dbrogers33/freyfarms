ff69c131495765e603250ed77ef6650549660173
Denham Rogers <denhamrogers10@gmail.com>
2025-11-07 11:55:35 -0600

    Fixed images

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,5 +6,16 @@
 
 // You can delete this file if you're not using it
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// gatsby-browser.js
+import "normalize.css"; // base first
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// optional: force layout settle on route change
+export const onRouteUpdate = () => {
+  // Nudge slick/gatsby-image after navigation
+  window.requestAnimationFrame(() => {
+    window.dispatchEvent(new Event("resize"));
+    setTimeout(() => window.dispatchEvent(new Event("resize")), 120);
+  });
+};
--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
-import 'normalize.css'
+
 
 const globalStyles = createGlobalStyle`
 
