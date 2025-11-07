7d250c5bbd2576a6be270ec07a59539c0dbf3a39
Denham Rogers <denhamrogers10@gmail.com>
2025-11-07 10:58:48 -0600

    added images to receipe catagory pages

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -28,7 +28,7 @@ const IndexPage = ({ data }) => (
         paragraph={<P>A cozy fall favorite, Buckskin Pumpkin Soup is a rich, velvety blend of roasted pumpkin, butter, and cream, lightly seasoned with cumin, salt, and pepper. Each bowl delivers warm, earthy flavors and smooth texture, finished with a dollop of sour cream for a creamy, comforting touch. Perfect for crisp evenings or festive holiday meals.</P>}
         images={[
           { fluid: data.buckskinOne.childImageSharp.fluid },
-          { fluid: data.buckskinTwo.childImageSharp.fluid},
+          { fluid: data.yellowMelons.childImageSharp.fluid},
         ]}
         alt="Seedless and Seeded Watermelons'"
       >
@@ -43,7 +43,11 @@ const IndexPage = ({ data }) => (
       <SideBySide reverse="row-reverse"
         headerTwo={<H2>Fairytale Stuffing</H2>}
         paragraph={<P>This Cinderella Pumpkin Stuffing recipe puts a cozy, seasonal twist on a classic favorite. Tender roasted pumpkin pieces are folded into buttery StoveTop stuffing made with a savory blend of chicken broth and spices. The result is a warm, flavorful side dish that balances sweet pumpkin with savory comfort—perfect for holiday gatherings or a hearty fall meal.</P>}
-        src={data.seedlessAndSeeded.childImageSharp.fluid}
+        images={[
+          { fluid: data.fairytaleStuffingOne.childImageSharp.fluid },
+          { fluid: data.fairytaleStuffingTwo.childImageSharp.fluid },
+          { fluid: data.fairytaleStuffingThree.childImageSharp.fluid },
+        ]}
         alt="Seedless and Seeded Watermelons'"
       >
         <Button
@@ -57,7 +61,11 @@ const IndexPage = ({ data }) => (
       <SideBySide reverse="row-reverse"
         headerTwo={<H2>Honeynut Feta Boats</H2>}
         paragraph={<P>These Honeynut Squash Ricotta Boats are a cozy, elegant side dish that’s both savory and slightly sweet. Roasted honeynut squash halves are filled with a creamy ricotta mixture seasoned with paprika, garlic, and onion powder, then warmed to perfection and topped with crunchy walnuts. A drizzle of olive oil and hint of cinnamon bring a touch of autumn comfort to every bite.</P>}
-        src={data.seedlessAndSeeded.childImageSharp.fluid}
+        images={[
+          { fluid: data.fetaBoatOne.childImageSharp.fluid },
+          { fluid: data.fetaBoatTwo.childImageSharp.fluid },
+          { fluid: data.fetaBoatThree.childImageSharp.fluid },
+        ]}
         alt="Seedless and Seeded Watermelons'"
       >
         <Button
@@ -67,11 +75,25 @@ const IndexPage = ({ data }) => (
       </SideBySide>
     </ProductWrapper>
 
+     <ProductWrapper>
+      <SideBySide reverse="row-reverse"
+        headerTwo={<H2>Marla's Pumpkin Rolls</H2>}
+        paragraph={<P>Soft, fluffy, and perfectly spiced — these pumpkin cinnamon rolls take the classic comfort of a cinnamon roll and elevate it with rich pumpkin flavor in every layer. The pumpkin purée adds moisture and warmth to the dough, while the spiced filling creates a cozy, aromatic sweetness. Finished with a smooth vanilla icing that melts over the warm rolls, they’re an irresistible fall favorite that’s just as perfect for holiday mornings as they are for everyday indulgence.</P>}
+        src={data.pumpkinrolls.childImageSharp.fluid }
+        alt="Seedless and Seeded Watermelons'"
+      >
+        <Button
+          link="/recipes/marlas-epic-pumpkin-rolls"
+          buttonLabel="View Recipe"
+        />
+      </SideBySide>
+    </ProductWrapper>
+
     <ProductWrapper>
       <SideBySide reverse="row-reverse"
         headerTwo={<H2>Seasoned Cinderella</H2>}
         paragraph={<P>Seasoned Cinderella Cubes are a flavorful and rustic side dish that highlights the natural sweetness of Cinderella pumpkin. Cubed pumpkin is tossed with melted butter, caramelized onions, garlic, and a blend of smoky paprika and earthy cumin, then finished with a sprinkle of fresh herbs. The result is a warm, savory, and slightly sweet dish that pairs beautifully with roasted meats or holiday favorites.</P>}
-        src={data.seedlessAndSeeded.childImageSharp.fluid}
+        images={[{ fluid: data.seasonedCinderella.childImageSharp.fluid }]}
         alt="Seedless and Seeded Watermelons'"
       >
         <Button
@@ -85,7 +107,7 @@ const IndexPage = ({ data }) => (
       <SideBySide reverse="row-reverse"
         headerTwo={<H2>Sweet Sliced Cushaw</H2>}
         paragraph={<P> Sweet Sliced Cushaw is a simple and nostalgic Southern-style side dish that highlights the natural sweetness of cushaw squash. Thinly sliced pieces are brushed with melted butter, sprinkled with brown sugar and cinnamon, and roasted until tender and caramelized. The result is a golden, fragrant dish that’s equal parts cozy dessert and comforting vegetable bake—perfect for fall gatherings or holiday meals.</P>}
-        src={data.seedlessAndSeeded.childImageSharp.fluid}
+        images={[{ fluid: data.slicedCushaw.childImageSharp.fluid }]}
         alt="Seedless and Seeded Watermelons'"
       >
         <Button
@@ -167,9 +189,72 @@ query {
       fluid(maxWidth: 750, quality: 100) {
         ...GatsbyImageSharpFluid_withWebp
       }
-    }
+    } 
   }
-  yellowMelons: file(relativePath: { eq: "products/yellow-melons.jpg" }) {
+  fairytaleStuffingOne: file(relativePath: { eq: "recipes/fairytale-stuffing/fairytale-stuffing-one.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  fairytaleStuffingTwo: file(relativePath: { eq: "recipes/fairytale-stuffing/fairytale-stuffing-two.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  fairytaleStuffingThree: file(relativePath: { eq: "recipes/fairytale-stuffing/fairytale-stuffing-three.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  pumpkinrolls: file(relativePath: { eq: "recipes/pumpkin-rolls/pumpkin-rolls.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  seasonedCinderella: file(relativePath: { eq: "recipes/seasoned-cinderella/seasoned-cinderella.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  slicedCushaw: file(relativePath: { eq: "recipes/sliced-cushaw/slicde-cushaw.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  fetaBoatOne: file(relativePath: { eq: "recipes/feta-boat/feta-boat-one.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  fetaBoatTwo: file(relativePath: { eq: "recipes/feta-boat/feta-boat-two.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  fetaBoatThree: file(relativePath: { eq: "recipes/feta-boat/feta-boat-three.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 750, quality: 100) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    } 
+  }
+  yellowMelons: file(relativePath: { eq: "recipes/buckskin/buckskin-2.jpg" }) {
     childImageSharp {
       fluid(maxWidth: 750, quality: 100) {
         ...GatsbyImageSharpFluid_withWebp
